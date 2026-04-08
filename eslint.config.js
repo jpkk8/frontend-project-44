41e111a3b87f0303ca235b04370d6b9259fd036a
karinabutyrkina <karinabutyrkina@gmail.com>
2026-04-08 22:27:00 +0300

    add sonarcloud badges and setup linter

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import stylistic from '@stylistic/eslint-plugin';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.js'],
+    plugins: {
+      '@stylistic': stylistic,
+    },
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.node,
+      },
+    },
+    rules: {
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'always'],
+      'no-unused-vars': 'warn',
+      'no-console': 'off',
+    },
+  },
+];
