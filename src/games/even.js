a3eca2cbf7b73e15e130b6277a2c27fb4218177c
karinabutyrkina <karinabutyrkina@gmail.com>
2026-04-19 23:45:26 +0300

    good

--- src/games/even.js
+++ src/games/even.js
@@ -1,21 +1,17 @@
-import runEngine from '../index.js';
+import runEngine from '../index.js'
 
-// правила игры
-const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+const description = 'Answer "yes" if the number is even, otherwise answer "no".'
 
-// Проверка на четность (предикат)
-const isEven = (num) => num % 2 === 0;
+const isEven = (num) => num % 2 === 0
 
 const getRoundData = () => {
-  const number = Math.floor(Math.random() * 100); // Случайное число от 0 до 99
-  const question = String(number);
-  const correctAnswer = isEven(number) ? 'yes' : 'no';
+  const number = Math.floor(Math.random() * 100)
+  const question = String(number)
+  const correctAnswer = isEven(number) ? 'yes' : 'no'
   
-  return [question, correctAnswer]; // Возвращаем вопрос и ответ в движок
-};
-
+  return [question, correctAnswer]}
 const runEven = () => {
-  runEngine(description, getRoundData);
-};
+  runEngine(description, getRoundData)
+}
 
-export default runEven;
+export default runEven
